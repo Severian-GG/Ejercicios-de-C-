2427da32fa0476698ca5e5f8db8597683592d46b
¨Severian-GG¨ <jorgegv1604@gmail.com>
2026-09-12 01:14:13 -0600

    Agrego primeros ejercicios en C++

---
+++ Lectura y entrada de datos.cpp
@@ -0,0 +1,19 @@
+//Lectura y entrada de datos
+//este programa permite ingresar un numero y mostrarlo en pantalla
+
+#include <iostream>
+
+using namespace std;
+
+int main() { /*se determina el tipo de variable primero y despues se asigna un valor */
+    float numero;
+
+    cout << "diga un numero decimal: ";
+    cin>> numero;
+
+    cout << "\el numero ingresado es: "<<numero;
+
+
+
+    return 0;
+}
---
+++ tipos de datos basicos en cpp.cpp
@@ -0,0 +1,17 @@
+//Tipos de datos básicos en C++
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int numero = 666;
+    float decimal = 6.66;
+    double mayor = 666.666;
+    char letra = 'a';
+
+    cout << letra;
+
+    return 0;
+    
+}
