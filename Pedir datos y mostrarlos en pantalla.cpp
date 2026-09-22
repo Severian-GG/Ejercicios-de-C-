f0c6f2c0e49b6dc10f801e9b9e8e87182f489823
¨Severian-GG¨ <jorgegv1604@gmail.com>
2026-09-22 17:32:01 -0600

    Ejercicio para pedir datos y mostrarlos en pantalla

---
+++ Pedir datos y mostrarlos en pantalla.cpp
@@ -0,0 +1,28 @@
+/* realice un programa que lea de la entrada estandar los siguientes datos 
+de una persona 
+
+    Edad: dato de tipo entero.
+    Sexo:dato de tipo caracter.
+    Altura en metros: dato del tipo real.
+    
+    Tras leer los datos, el programa debe mostrarlos en la salida estandar. */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int edad;
+    char sexo[10];
+    float altura;
+
+cout << "digite su edad: "; cin>>edad; 
+cout << "digite su sexo: "; cin>>sexo;
+cout << "digite su altura en metros: "; cin>>altura;
+
+cout<<"\nEdad "<<edad<<endl;
+cout<<"Sexo "<<sexo<<endl;
+cout<<"Altura en metros "<<altura<<endl;
+
+    return 0;
+}   
